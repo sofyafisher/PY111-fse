a1d57260a44e58cf6c2ecc9e6e9f1f1a732eb132
fisch <python0fishers@gmail.com>
2022-09-16 16:56:48 +0300

    b2 done

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -1,7 +1,17 @@
 """
 Taylor series
 """
+import math
 from typing import Union
+ERR = 0.000001
+
+def get_value_x(x, n):
+  value = ((-1) ** (n-1) * x ** (2 * n - 1)) / math.factorial(2* n - 1)
+  return value
+
+def get_value_e(x, n):
+  value = (x**n)/math.factorial(n)
+  return value
 
 
 def ex(x: Union[int, float]) -> float:
@@ -11,8 +21,14 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    n = 0
+    exx = 0
+    while abs(get_value_e(x, n)) > ERR:
+        exx += get_value_e(x, n)
+        n += 1
+        if abs(get_value_e(x, n)) <= ERR:
+            return exx
+    return None
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -22,5 +38,11 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    n = 1
+    sinn = 0
+    while abs(get_value_x(x, n)) > ERR:
+        sinn += get_value_x(x, n)
+        n += 1
+        if abs(get_value_x(x, n)) <= ERR:
+            return sinn
+    return None
